f79e23b7c06430afe3bad86d9ab222521036d9f5
VitorDalBelo <vitoralmeida721@gmail.com>
2024-04-26 22:46:55 -0300

    ajust

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -7,17 +7,7 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 export class DatabaseConfigService implements TypeOrmOptionsFactory{
     constructor(private configService: ConfigService){}
     createTypeOrmOptions(connectionName?: string): TypeOrmModuleOptions {
-        console.log({            type:"postgres",
-        host:process.env.DB_HOST,
-        port:Number(process.env.DB_PORT),
-        username:process.env.DB_USER,
-        password:String(process.env.DB_PASSWORD),
-        database:process.env.DB_NAME,
-        entities:[__dirname + '/../**/*.entity{.js,ts}'],
-        logging:false,
-        synchronize:process.env.DEVELOPMENT === 'true',
-        schema:"public",
-        verboseRetryLog:true,})
+
         return{
             type:"postgres",
             host:process.env.DB_HOST,
